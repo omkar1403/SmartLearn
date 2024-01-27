8431f73d60e1f62c496a9af2be53bdcca0683866
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-01-27 17:46:07 +0530

    edited user database

--- coursecrazeserver/models/User.js
+++ coursecrazeserver/models/User.js
@@ -1,74 +1,55 @@
 import mongoose from "mongoose";
 import validator from "validator";
 
-const schema = new mongoose.Schema(
-    {
-        name: {
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter valid name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter your Email"],
+        unique: true,
+        validate: validator.isEmail,
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter your Password"],
+        minLength: [6, "Password must be at least 6 characters"],
+        select: false,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user", 
+    },
+    Subscription: {
+        id: String,
+        status: String,
+    },
+    avatar: {
+        public_id: {
             type: String,
-            required: [true, "Please enter valid name"],
+            required: true, // Corrected placement of required
         },
-        email: {
+        url: {
             type: String,
-            required: [true, "Please Enter your Email"],
-            unique: true,
-            validate: validator.isEmail,
-
+            required: true,
         },
-        password: {
-            type: String,
-            required: [true, "Please Enter your Password"],
-            minLength: [6, "Password must be at least 6 characters"],
-            select: false,
-
+    },
+    playlist: [{
+        course: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course",
         },
-
-        role: {
-            type: String,
-            enum: ["admin", "user"],
-            dafault: "user",
-
-        },
-
-        Subscription: {
-            id: String,
-            status: String,
-        },
-        avatar: {
-            public_id: {
-                status: String,
-                required: true,
-            },
-            url: {
-                type: String,
-                required: true,
-            },
-        },
-
-
-        playlist: [{
-            course: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Course",
-            },
-
-            poster: String,
-
-        },
-        ],
-
-
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-
-
-        ResetPasswordToken: String,
-        ResetPasswordExpire: String,
-
-
-    }
-
-);
+        poster: String,
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    ResetPasswordToken: String,
+    ResetPasswordExpire: String,
+});
 
 export const User = mongoose.model("User", schema);
